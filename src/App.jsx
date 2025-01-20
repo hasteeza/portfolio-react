124b159f45e19d792a2de87ce0800b4e44448a93
Terence Dave Natad <terencedavenatad@gmail.com>
2025-01-20 18:43:36 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+
+import { Qwerty } from './components/navbar'
 
 function App() {
-  const [name, setName] = useState("Kylo Ren")
-
-  function handleClick(){
-    setName("Darth Revan")
-  }
 
   return (
     <>
-    <button className="bg-slate-600 p-10 hover:bg-slate-700" onClick={handleClick}>Click Me!</button>
-    {name}
+    <Qwerty />
+   
     </>
   )
 }
